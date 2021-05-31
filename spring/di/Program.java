647bf634281fef64e9eccf0f86feb88454f903a1
Youngrae Cho <83386000+Ray901104@users.noreply.github.com>
2021-05-31 13:52:44 +0900

    Update Program.java
    스프링에게 DI를 지시하는 방법으로 코드를 변경

--- spring/di/Program.java
+++ spring/di/Program.java
@@ -1,18 +1,42 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.di.entity.Exam;
 import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
 import spring.di.ui.GridExamConsole;
-import spring.di.ui.InlineExamConsole;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Exam exam = new NewlecExam();
-//		ExamConsole console = new InlineExamConsole(exam);//DI¸¦ Á÷Á¢ ÇÑ °Í
-		ExamConsole console = new GridExamConsole(exam);//±ÍÂúÀºÀÏÀ» ´ë½ÅÇØÁÖ´Â °ÍÀÌ Spring
+		/* ìŠ¤í”„ë§ì—ê²Œ ì§€ì‹œí•˜ëŠ” ë°©ë²•ìœ¼ë¡œ ì½”ë“œë¥¼ ë³€ê²½
+		//Exam exam = new NewlecExam();
+		Exam exam = new NewlecExam(10, 10, 10, 10);
+		ExamConsole console = new GridExamConsole();
+
+		console.setExam(exam);
+		*/
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+//		Exam exam = context.getBean(Exam.class);
+//		System.out.println(exam.toString());
+		ExamConsole console = (ExamConsole) context.getBean("console");
+//		ExamConsole console = context.getBean(ExamConsole.class);
+		
 		console.print();
+		
+//		List<Exam> exams = (List<Exam>) context.getBean("exams"); //new ArrayList<>();
+//		exams.add(new NewlecExam(1, 1, 1, 1));
+		
+//		for(Exam e : exams) {
+//			System.out.println(e);
+//		}//end for
 	}//main
 
 }//class
